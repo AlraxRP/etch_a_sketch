3b25926485fa29dee2b19914e8c85fa24a2531d7
Alexis <alexisrp_4@hotmail.com>
2026-01-20 21:11:52 -0600

    Add button to change the size of the grid
    The implemented button sends the user a popup asking for
    the number of squares per side for the new grid. The existing grid is
    removed, and a new grid is generated.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
+function createGrid(SquarePerSide)
+{
+    for(let i = 0; i<(SquarePerSide*SquarePerSide); i++)
+    {
+        const square = document.createElement("div");
+            
+        square.classList.add("square");
+
+        container.appendChild(square);
+
+        square.addEventListener("mouseenter", () => {
+                square.style.backgroundColor = "red";
+        });
+    }
+}
+
+
 const container = document.querySelector("#container");
 
-for(let i = 0; i<320; i++)
-{
-    const square = document.createElement("div");
-    
-    square.classList.add("square");
+const btnChangeSize = document.querySelector("#btnChangeSize");
 
-    container.appendChild(square);
+btnChangeSize.addEventListener("click", () => {
 
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "red";
-    });
-};
+    let newSize = prompt("Enter new grid size:",16);
+
+    if(newSize <= 100)
+    {
+        while(container.firstChild)
+        {
+            container.removeChild(container.firstChild);
+        }
+
+        createGrid(newSize);
+
+        for(let square of container.children)
+        {
+            square.style.flexBasis = `calc(100% / ${newSize})`;
+        }
+    }
+    else
+    {
+        alert("Size to big, the grid can be only 100 squares or less per side");
+    }
+
+});
+
+createGrid(16);
